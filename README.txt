542a917e8c9f24d600a7e3a9aa4859b74828de0e
kaligone <badgujarpratik21@gmail.com>
2022-07-19 17:44:37 +0530

    First commit

---
+++ README.txt
@@ -0,0 +1,30 @@
+*************************************OSINT CTF******************************************
+OSINT CTF consit of 4 levels based on different difficulty levels
+
+
+
+***********************************Development******************************************
+The entire CTF is created by using the FLASK framework in python , HTML and CSS
+And run on the local server
+
+*****************************Access CTF via link****************************************
+CTF is also hosted on the link : https://osint-ctf.herokuapp.com/
+
+
+
+**********************************How to PLay CTF***************************************
+
+To run the CTF download the .zip file attached and unzip it.
+After unzip open the CMD and use following command to install all the requirement to run on local machines
+	C:\Users\Desktop\OSINT-CTF> pip3 install -r requirements.txt
+and install all the requirement needed to run the ctf
+After installing all the requirements go the OSINT-CTF folder then go to the __pycache__ folder and double
+click on the app.cpython-310.py to run the ctf and then go to the link shown after running in cmd
+OR
+
+Go to the CMD
+Then go the OSINT-CTF folder
+the in cmd type
+C:\Users\Desktop\OSINT-CTF> python3 app.py and it will run the local server 
+then go to the url shown like http://127.0.0.1:5000/
+and start solving
---
+++ app.py
@@ -0,0 +1,79 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import *
+import os
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/', methods = ["GET", "POST"])
+# ‘/’ URL is bound with hello_world() function.
+def welcome():
+	return render_template('welcome.html')
+
+@app.route('/Level_1', methods = ["GET", "POST"])
+def Level_1():
+	if request.method == 'POST':
+		flag_value1= request.form.get("flag")
+		if flag_value1 == 'OSINT.ctf{hawaii}':
+			flash("Boom !! You got the flag . Solve next level")
+			return redirect(url_for('Level_2'))
+		if flag_value1 and flag_value1 != 'OSINT.ctf{hawaii}':
+			error = "Wrong flag"
+			flash(error)
+
+	return render_template('Level_1.html')
+
+@app.route('/Level_2', methods = ["GET", "POST"])
+def Level_2():
+	if request.method == 'POST':
+		flag_value2 = request.form.get("flag2")
+		if flag_value2 == 'OSINT.ctf{14.4.31.3}':
+			flash("Boom !! You got the flag . Solve next level")
+			return redirect(url_for('Level_3'))
+		if flag_value2 and flag_value2 != 'OSINT.ctf{14.4.31.3}':
+			error = "Wrong flag"
+			flash(error)
+	return render_template('Level_2.html')
+
+
+@app.route('/Level_3', methods = ["GET", "POST"])
+def Level_3():
+	if request.method == 'POST':
+		flag_value3 = request.form.get("flag3")
+		if flag_value3 == 'OSINT.ctf{Ryuk}':
+			flash("Boom !! You got the flag . Solve next level")
+			return redirect(url_for('final'))
+		if flag_value3 and flag_value3 != 'OSINT.ctf{Ryu}':
+			error = "Wrong flag"
+			flash(error)
+	return render_template('Level_3.html')
+
+
+@app.route('/final', methods = ["GET", "POST"])
+def final():
+	if request.method == 'POST':
+		flag_value4 = request.form.get("flag4")
+		if flag_value4 == 'OSINT.ctf{national.museum.ireland}':
+			flash("Boom !! You got the flag . Solve next level")
+			return redirect(url_for('success'))
+		if flag_value4 and flag_value4 != 'OSINT.ctf{national.museum.ireland}':
+			error = "Wrong flag"
+			flash(error)
+	return render_template('final.html')
+
+@app.route('/success', methods = ["GET", "POST"])
+def success():
+	if request.method == 'POST':
+		pass
+	return render_template('success.html')
+
+# main driver function
+if __name__ == '__main__':
+	app.config['SECRET_KEY']='osintctf'
+	#port = int(os.environ.get('PORT', 33507))
+	#waitress.serve(app, port=port)
+	app.run(debug=False)
